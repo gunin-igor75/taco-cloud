78637688bbbbcb2fdae94c5435dbb30cb210994a
Igor Gunin <gunin-igor75@yandex.ru>
2023-06-30 02:39:38 +0600

    add branch jpa

--- src/main/java/ru/gil/tacocloud/model/Ingredient.java
+++ src/main/java/ru/gil/tacocloud/model/Ingredient.java
@@ -3,11 +3,15 @@ package ru.gil.tacocloud.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
+
 @Data
-@Table
-public class Ingredient {
+@Table("ingredient")
+public class Ingredient  implements Persistable<String> {
 
     @Id
     private final String id;
@@ -15,6 +19,34 @@ public class Ingredient {
     private final String name;
 
     private final Type type;
+
+    @Transient
+    private final boolean isNew;
+
+
+
+
+    public Ingredient(String id, String name, Type type, boolean isNew) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.isNew = isNew;
+    }
+
+    @PersistenceCreator
+    public Ingredient(String id, String name, Type type) {
+        this(id, name, type, false);
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE;
